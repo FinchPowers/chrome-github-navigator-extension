a423d1e0234f771ec47f6d9402f4029f470bfae3
FinchPowers <fm.lheureux@gmail.com>
2014-03-03 23:34:18 -0500

    Click support, back + forward support

--- extension/github-page.js
+++ extension/github-page.js
@@ -6,6 +6,93 @@ function cleanFilesList(fileList){
     }
 }
 
+function updateUrl(anchor){
+    window.history.pushState(
+        {"ghNavigatorUrl" : $(anchor).attr("href")},
+        $(anchor).attr("title"),
+        $(anchor).attr("href"))
+}
+
+function fileBrowserOnClick(){
+    fileAction(this, true);
+    return false;
+}
+
+function fileAction(anchor, pushState){
+    var icon_span = $(anchor).parents("tr:first").find("td:first span");
+    var href = $(anchor).attr("href");
+    if ($(icon_span).hasClass("octicon-file-directory")){
+        if($(anchor).parents("tr:first").next().hasClass("ghExtensionExtend")){
+            //Already exists
+            $(anchor).parents("tr:first").next().toggle();
+        }else{
+            //First time, fetch it
+            $(anchor).parents("tr:first").after(
+                "<tr class='ghExtensionExtend'>"
+                + "<td colspan=4 style='padding-left: 10px;'>Loading</td>"
+                + "</tr>");
+            $.get(href, function(html){
+
+                html = $(html).find(".bubble.files-bubble");
+                cleanFilesList(html);
+                text = $(html).html();
+                $(anchor).parents("tr:first").next().find("td").html(text);
+            });
+        }
+    } else if ($(icon_span).hasClass("octicon-file-submodule")) {
+        //Nav out to the submodule
+        return true;
+    } else {
+        if(pushState){
+            updateUrl(anchor);
+        }
+        $(".githubPagesHighlighted").removeClass("githubPagesHighlighted");
+        $(anchor).parents("tr:first").addClass("githubPagesHighlighted");
+        if($(anchor).data("cachedFile")){
+            $("#fileViewer").find("table").remove();
+            $("#fileViewer").html(
+                "<table>" + $(anchor).data("cachedFile") + "</table>");
+        }else{
+            $("#fileViewer").html("Loading");
+            $.get(href, function(html){
+                var text;
+                if($(html).find(".file-code.file-diff.tab-size-8").size()){
+                    //standard code viewing pane
+                    text = $(html).find(".file-code.file-diff.tab-size-8")
+                           .html();
+                }else if($(html).find(".blob.instapaper_body").size()){
+                    //markdown
+                    text = $(html).find(".blob.instapaper_body").html();
+                }else if($(html).find(".blob-wrapper").size()){
+                    //images
+                    text = $(html).find(".blob-wrapper").html();
+                }else{
+                    throw "Cannot handle the selected resource";
+                }
+                $("#fileViewer").html("<table>" + text + "</table>");
+                $(anchor).data("cachedFile", text);
+            });
+        }
+    }
+}
+
+function disableFileBrowser(){
+    $("#fileBrowser, #fileViewer").hide();
+    $(".wrapper, .container").show();
+}
+function enableFileBrowser(){
+    $("#fileBrowser, #fileViewer").show();
+    $(".wrapper, .container").hide();
+}
+
+function toggleFileBrowser(){
+    if($("#fileBrowser:visible").size()){
+        disableFileBrowser();
+    }else{
+        enableFileBrowser();
+    }
+}
+
 function init(){
     $(".wrapper, .container").hide();
     $("body").append(
@@ -16,73 +103,24 @@ function init(){
     cleanFilesList($("#fileBrowser"));
     $("#fileBrowser").parents().css("height", "100%");
 
-    $("#fileBrowser").on("click", "a", function(){
-        var icon_span = $(this).parents("tr:first").find("td:first span");
-        var href = $(this).attr("href");
-        var anchor = this
-        if ($(icon_span).hasClass("octicon-file-directory")){
-            if($(anchor).parents("tr:first").next().hasClass("ghExtensionExtend")){
-                //Already exists
-                $(anchor).parents("tr:first").next().toggle();
-            }else{
-                //First time, fetch it
-                $(anchor).parents("tr:first").after(
-                    "<tr class='ghExtensionExtend'>"
-                    + "<td colspan=4 style='padding-left: 10px;'>Loading</td>"
-                    + "</tr>");
-                $.get(href, function(html){
-
-                    html = $(html).find(".bubble.files-bubble");
-                    cleanFilesList(html);
-                    text = $(html).html();
-                    $(anchor).parents("tr:first").next().find("td").html(text);
-                });
+    $("#fileBrowser").on("click", "a", fileBrowserOnClick);
+    window.onpopstate = function(event){
+        if(event.state == null || event.state["ghNavigatorUrl"] == undefined){
+            disableFileBrowser();
+        }else{
+            if(!$("#fileBrowser:visible").size()){
+                enableFileBrowser();
             }
-        } else if ($(icon_span).hasClass("octicon-file-submodule")) {
-            //Nav out to the submodule
-            return true;
-        } else {
-            $(".githubPagesHighlighted").removeClass("githubPagesHighlighted");
-            $(this).parents("tr:first").addClass("githubPagesHighlighted");
-            if($(this).data("cachedFile")){
-                $("#fileViewer").find("table").remove();
-                $("#fileViewer").html(
-                    "<table>" + $(this).data("cachedFile") + "</table>");
-            }else{
-                var anchor = this;
-                $("#fileViewer").html("Loading");
-                $.get(href, function(html){
-                    var text;
-                    if($(html).find(".file-code.file-diff.tab-size-8").size()){
-                        //standard code viewing pane
-                        text = $(html).find(".file-code.file-diff.tab-size-8")
-                               .html();
-                    }else if($(html).find(".blob.instapaper_body").size()){
-                        //markdown
-                        text = $(html).find(".blob.instapaper_body").html();
-                    }else if($(html).find(".blob-wrapper").size()){
-                        //images
-                        text = $(html).find(".blob-wrapper").html();
-                    }else{
-                        throw "Cannot handle the selected resource";
-                    }
-                    $("#fileViewer").html("<table>" + text + "</table>");
-                    $(anchor).data("cachedFile", text);
-                });
-            }
+            url = event.state["ghNavigatorUrl"].replace(/\//g, "\\/")
+                .replace(/\./g, "\\.");
+            fileAction($("#fileBrowser").find("a[href=" + url + "]"), false);
         }
-        return false;
-    });
+    }
 }
 
+
 if($("#fileBrowser").size()){
-    if($("#fileBrowser:visible").size()){
-        $("#fileBrowser, #fileViewer").hide();
-        $(".wrapper, .container").show();
-    }else{
-        $("#fileBrowser, #fileViewer").show();
-        $(".wrapper, .container").hide();
-    }
+    toggleFileBrowser();
 }else{
     init();
 }
